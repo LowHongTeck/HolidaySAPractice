ef2b5271c96c55513bbbea02c9dc05efe31d29b7
LowHongTeck <20043679@myrp.edu.sg>
2021-07-03 13:49:38 +0800

    .....

--- app/src/main/java/sg/edu/rp/c346/id20043679/holidaysapractice/ItemListActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20043679/holidaysapractice/ItemListActivity.java
@@ -16,6 +16,7 @@ import android.widget.ListView;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ItemListActivity extends AppCompatActivity {
 
@@ -26,6 +27,7 @@ public class ItemListActivity extends AppCompatActivity {
 
     DatePicker dp;
     ArrayList<String> alItems;
+    ArrayList<String> unsortedALItems;
 
 
     int itemClicked;
@@ -45,19 +47,37 @@ public class ItemListActivity extends AppCompatActivity {
         dp = findViewById(R.id.datePicker);
 
         alItems = new ArrayList<String>();
+        unsortedALItems = new ArrayList<>();
 
         aaItems = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alItems);
         lvItems.setAdapter(aaItems);
 
         registerForContextMenu(lvItems);
 
+        alItems.add("Expires 2021-5-4 Lenovo");
+        alItems.add("Expires 2021-6-8 Asus");
+        alItems.add("Expires 2021-7-9 Acer");
+        alItems.add("Expires 2021-9-10 HP");
+        alItems.add("Expires 2021-10-1 Dell");
+
+//        unsortedALItems.add("Lenovo");
+//        unsortedALItems.add("Asus");
+//        unsortedALItems.add("Acer");
+//        unsortedALItems.add("HP");
+//        unsortedALItems.add("Dell");
+
+        Collections.sort(unsortedALItems, String.CASE_INSENSITIVE_ORDER);
+        aaItems.notifyDataSetChanged();
+
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int m = dp.getMonth() + 1;
                 String item = etNewItem.getText().toString();
-                String aItem = "Expires " + dp.getYear() + "-" + dp.getMonth()
+                String aItem = "Expires " + dp.getYear() + "-" + m
                         + "-" + dp.getDayOfMonth() + " " + item;
                 alItems.add(aItem);
+                Collections.sort(alItems, String.CASE_INSENSITIVE_ORDER);
                 aaItems.notifyDataSetChanged();
             }
         });
@@ -74,12 +94,15 @@ public class ItemListActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 switch(position){
                     case 0:
-                        eMonth = 1;
+                        eMonth = 0;
                         break;
                     case 1:
-                        eMonth = 3;
+                        eMonth = 1;
                         break;
                     case 2:
+                        eMonth = 3;
+                        break;
+                    case 3:
                         eMonth = 6;
                         break;
                 }
@@ -109,10 +132,16 @@ public class ItemListActivity extends AppCompatActivity {
 
     @Override
     public boolean onContextItemSelected(@NonNull MenuItem item) {
+
         if(item.getItemId()==0){
+            int m = dp.getMonth() + 1;
             String nItem = etNewItem.getText().toString();
-            alItems.set(itemClicked, "Expires " + dp.getYear() + "-" + dp.getMonth()
+            alItems.set(itemClicked, "Expires " + dp.getYear() + "-" + m
                     + "-" + dp.getDayOfMonth() + " " + nItem);
+
+            aaItems.notifyDataSetChanged();
+        } else if (item.getItemId()==1){
+            alItems.remove(itemClicked);
 
             aaItems.notifyDataSetChanged();
         }
